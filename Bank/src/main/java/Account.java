faa0533d18dd0b73e9b3d0c93173b4a04d06a585
guscaxile <gustavocaxile@outlook.com>
2023-07-28 15:31:23 -0300

    first datas

---
+++ Bank/src/main/java/Account.java
@@ -0,0 +1,14 @@
+public class Account {
+
+    private String ag;
+    private String cc;
+    private String name;
+
+    static int MAX_LENGTH = 12;
+
+    public Account(String ag, String cc, String name) {
+        this.ag = ag;
+        this.cc = cc;
+        this.name = name;
+    }
+}
--- Bank/src/main/java/App.java
+++ Bank/src/main/java/App.java
@@ -2,6 +2,8 @@ public class App {
 
     public static void main(String[] args) {
 
+        Account account = new Account("0001", "1234", "Gustavo");
+
         
     }
 }
